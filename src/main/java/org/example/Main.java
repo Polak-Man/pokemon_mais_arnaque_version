0c124a5a076479717313da458b82f5cc4b3976a1
Polak-Man <logan.vourey@ecoles-epsi.net>
2024-12-03 09:36:05 +0100

    v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Ouais ça part ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Saisir identifiant : ");
+        String identifiant = scanner.nextLine();
+
+        System.out.print("Saisir mot de passe : ");
+        String motDePasse = scanner.nextLine();
+
+        // Traitement des données utilisateur (ici, simple affichage)
+        System.out.println("Identifiant : " + identifiant);
+        System.out.println("Mot de passe : " + motDePasse);
+
+        scanner.close();
     }
 }
