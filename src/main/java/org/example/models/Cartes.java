c8517268fd28d3c0895e997ab7accaccb9f47205
Polak-Man <logan.vourey@ecoles-epsi.net>
2024-12-03 10:16:44 +0100

    creation de la classe Cartes et affichage du contenu dans le main

---
+++ src/main/java/org/example/models/Cartes.java
@@ -0,0 +1,51 @@
+package org.example.models;
+
+public class Cartes {
+    // Attributs
+    private String type;
+    private String rarete;
+    private String nom;
+
+    // Constructeur
+    public Cartes(String type, String rarete, String nom) {
+        this.type = type;
+        this.rarete = rarete;
+        this.nom = nom;
+    }
+
+    // Getters
+    public String getType() {
+        return type;
+    }
+
+    public String getRarete() {
+        return rarete;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    // Setters
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setRarete(String rarete) {
+        this.rarete = rarete;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    // Méthode toString
+    @Override
+    public String toString() {
+        return "Carte{" +
+                "type='" + type + '\'' +
+                ", rarete='" + rarete + '\'' +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
